37823a7f9a9230dd47bd350c06df68d1c425e4d9
Ognyan Stoyanov <o.r.stoyanov@gmail.com>
2022-10-11 20:25:14 +0300

    11.10/20:25

---
+++ homework/css-homework-L4/set-event-name.js
@@ -0,0 +1,19 @@
+/* To change the name of event just change the content between double quotes */
+
+$('h2').text("Event name");
+
+/* If you want to change the Tags */
+
+$('.tag-one').text("Tag 1");
+$('.tag-two').text("Tag 2");
+$('.tag-three').text("Tag 3");
+$('.tag-four').text("Tag 4");
+
+
+/* If you want to change suitable for: please change visitors with your preferences */
+
+$('.suit-one').text("Visitor 1");
+$('.suit-two').text("Visitor 2");
+$('.suit-three').text("Visitor 3");
+
+
